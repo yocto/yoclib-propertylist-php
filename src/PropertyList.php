8a61dcb1a5d5c779e61bedcf48b4fd99d59e83f9
Ben <ben@yocto.nu>
2023-05-26 11:48:37 +0200

    Implement serialization

--- src/PropertyList.php
+++ src/PropertyList.php
@@ -1,6 +1,8 @@
 <?php
 namespace YOCLIB\PropertyList;
 
+use DateTimeInterface;
+
 class PropertyList{
 
     public const FLAG_FORCE_QUOTATION_MARKS = 'force_quotation_marks';
@@ -12,7 +14,12 @@ class PropertyList{
     public const FORMAT_BINARY = 'binary';
     public const FORMAT_JSON = 'json';
 
+    public const TYPE_ARRAY = 'array';
     public const TYPE_BOOLEAN = 'boolean';
+    public const TYPE_DATE = 'date';
+    public const TYPE_DICTIONARY = 'dictionary';
+    public const TYPE_INTEGER = 'integer';
+    public const TYPE_REAL = 'real';
     public const TYPE_STRING = 'string';
 
     public const VERSION_1_0 = '1.0';
@@ -68,7 +75,22 @@ class PropertyList{
     }
 
     private static function isType($object,$type){
+        if($type===self::TYPE_ARRAY && is_array($object) && self::array_is_list($object)){
+            return true;
+        }
         if($type===self::TYPE_BOOLEAN && is_bool($object)){
+            return true;
+        }
+        if($type===self::TYPE_DATE && $object instanceof DateTimeInterface){
+            return true;
+        }
+        if($type===self::TYPE_DICTIONARY && (is_object($object) || (is_array($object) && !self::array_is_list($object)))){
+            return true;
+        }
+        if($type===self::TYPE_INTEGER && is_integer($object)){
+            return true;
+        }
+        if($type===self::TYPE_REAL && is_float($object)){
             return true;
         }
         if($type===self::TYPE_STRING && is_string($object)){
@@ -77,7 +99,46 @@ class PropertyList{
         return false;
     }
 
+    /**
+     * @param mixed|DateTimeInterface $object
+     * @param $format
+     * @param $flags
+     * @return false|string
+     */
     private static function serializeObject($object,$format,$flags=0){
+        if(self::isType($object,self::TYPE_ARRAY)){
+            if($format===self::FORMAT_ASCII){
+                $arr = [];
+                foreach($object AS $item){
+                    $arr[] = self::serializeObject($item,$format,$flags);
+                }
+                return '('.implode(', ',$arr).')';
+            }
+            if($format===self::FORMAT_ASCII_GNUSTEP){
+                $arr = [];
+                foreach($object AS $item){
+                    $arr[] = self::serializeObject($item,$format,$flags);
+                }
+                return '('.implode(', ',$arr).')';
+            }
+            //TODO GNU Binary
+            if($format===self::FORMAT_XML){
+                $arr = '<array>';
+                foreach($object AS $item){
+                    $arr .= self::serializeObject($item,$format,$flags);
+                }
+                $arr .= '</array>';
+                return $arr;
+            }
+            //TODO Binary
+            if($format===self::FORMAT_JSON){
+                $arr = [];
+                foreach($object AS $item){
+                    $arr[] = json_decode(self::serializeObject($item,$format,$flags));
+                }
+                return json_encode($arr);
+            }
+        }
         if(self::isType($object,self::TYPE_BOOLEAN)){
             if($format===self::FORMAT_ASCII){
                 //TODO Fix flag: Force Quotation Marks
@@ -90,6 +151,98 @@ class PropertyList{
             //TODO GNU Binary
             if($format===self::FORMAT_XML){
                 return $object?'<true/>':'<false/>';
+            }
+            //TODO Binary
+            if($format===self::FORMAT_JSON){
+                return json_encode($object);
+            }
+        }
+        if(self::isType($object,self::TYPE_DATE)){
+            if($format===self::FORMAT_ASCII){
+                //TODO Fix flag: Force Quotation Marks
+                //TODO String casted date format
+                $timezone = $object->format('O');
+                return '"'.$object->format('Y-m-d H:i:s ').($timezone==='+0000'?'Z':$timezone).'"';
+            }
+            if($format===self::FORMAT_ASCII_GNUSTEP){
+                $timezone = $object->format('O');
+                return '<*D'.$object->format('Y-m-d H:i:s ').($timezone==='+0000'?'Z':$timezone).'>';
+            }
+            //TODO GNU Binary
+            if($format===self::FORMAT_XML){
+                $timezone = $object->format('O');
+                return '<date>'.$object->format('Y-m-d\\TH:i:s').($timezone==='+0000'?'Z':$timezone).'</date>';
+            }
+            //TODO Binary
+            if($format===self::FORMAT_JSON){
+                $timezone = $object->format('O');
+                return json_encode($object->format('Y-m-d\\TH:i:s').($timezone==='+0000'?'Z':$timezone));
+            }
+        }
+        if(self::isType($object,self::TYPE_DICTIONARY)){
+            if($format===self::FORMAT_ASCII){
+                $dict = [];
+                foreach($object AS $key=>$item){
+                    $dict[] = self::serializeObject($key,$format,$flags).' = '.self::serializeObject($item,$format,$flags).';';
+                }
+                return '{'.implode(' ',$dict).'}';
+            }
+            if($format===self::FORMAT_ASCII_GNUSTEP){
+                $dict = [];
+                foreach($object AS $key=>$item){
+                    $dict[] = self::serializeObject($key,$format,$flags).' = '.self::serializeObject($item,$format,$flags).';';
+                }
+                return '{'.implode(' ',$dict).'}';
+            }
+            //TODO GNU Binary
+            if($format===self::FORMAT_XML){
+                $dict = '<dict>';
+                foreach($object AS $key=>$item){
+                    $dict .= '<key>'.htmlentities($key).'</key>';
+                    $dict .= self::serializeObject($item,$format,$flags);
+                }
+                $dict .= '</dict>';
+                return $dict;
+            }
+            //TODO Binary
+            if($format===self::FORMAT_JSON){
+                $dict = [];
+                foreach($object AS $key=>$item){
+                    $dict[$key] = json_decode(self::serializeObject($item,$format,$flags));
+                }
+                return json_encode((object) $dict);
+            }
+        }
+        if(self::isType($object,self::TYPE_INTEGER)){
+            if($format===self::FORMAT_ASCII){
+                //TODO Fix flag: Force Quotation Marks
+                return '"'.$object.'"';
+            }
+            if($format===self::FORMAT_ASCII_GNUSTEP){
+                //TODO Fix flag: Force Quotation Marks
+                return '<*I'.$object.'>';
+            }
+            //TODO GNU Binary
+            if($format===self::FORMAT_XML){
+                return '<integer>'.htmlentities($object).'</integer>';
+            }
+            //TODO Binary
+            if($format===self::FORMAT_JSON){
+                return json_encode($object);
+            }
+        }
+        if(self::isType($object,self::TYPE_REAL)){
+            if($format===self::FORMAT_ASCII){
+                //TODO Fix flag: Force Quotation Marks
+                return '"'.$object.'"';
+            }
+            if($format===self::FORMAT_ASCII_GNUSTEP){
+                //TODO Fix flag: Force Quotation Marks
+                return '<*R'.$object.'>';
+            }
+            //TODO GNU Binary
+            if($format===self::FORMAT_XML){
+                return '<real>'.htmlentities($object).'</real>';
             }
             //TODO Binary
             if($format===self::FORMAT_JSON){
@@ -115,6 +268,20 @@ class PropertyList{
             }
         }
         return '';
+    }
+
+    /**
+     * @param array $arr
+     * @return bool
+     */
+    private static function array_is_list(array $arr): bool{
+        if(function_exists('array_is_list')){
+            return array_is_list($arr);
+        }
+        if($arr===[]){
+            return true;
+        }
+        return array_keys($arr)===range(0,count($arr)-1);
     }
 
 }
--- tests/PropertyListTest.php
+++ tests/PropertyListTest.php
@@ -1,12 +1,16 @@
 <?php
 namespace YOCLIB\PropertyList\Tests;
 
+use DateTime;
+use stdClass;
+
 use PHPUnit\Framework\TestCase;
+
 use YOCLIB\PropertyList\PropertyList;
 
 class PropertyListTest extends TestCase{
 
-    public function testEmptyPropertyList(){
+    public function testSerializingEmptyPropertyList(){
         $plist = new PropertyList;
 
         self::assertEquals('',$plist->serialize(PropertyList::FORMAT_ASCII));
@@ -17,7 +21,103 @@ class PropertyListTest extends TestCase{
         self::assertEquals('',$plist->serialize(PropertyList::FORMAT_JSON));
     }
 
-    public function testPropertyListWithString(){
+    public function testSerializingPropertyListWithEmptyArray(){
+        $plist = new PropertyList;
+        $plist->setObject([]);
+
+        self::assertEquals('()',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('()',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><array></array></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('[]',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithEmptyObject(){
+        $plist = new PropertyList;
+        $plist->setObject(new stdClass);
+
+        self::assertEquals('{}',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('{}',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><dict></dict></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('{}',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithEmptyString(){
+        $plist = new PropertyList;
+        $plist->setObject('');
+
+        self::assertEquals('""',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('""',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><string></string></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('""',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithBooleanTrue(){
+        $plist = new PropertyList;
+        $plist->setObject(true);
+
+        self::assertEquals('"true"',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('<*BY>',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><true/></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('true',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithBooleanFalse(){
+        $plist = new PropertyList;
+        $plist->setObject(false);
+
+        self::assertEquals('"false"',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('<*BN>',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><false/></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('false',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithDate(){
+        $plist = new PropertyList;
+        $plist->setObject(new DateTime('2023-05-25 15:16:17 +00:00'));
+
+        self::assertEquals('"2023-05-25 15:16:17 Z"',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('<*D2023-05-25 15:16:17 Z>',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><date>2023-05-25T15:16:17Z</date></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('"2023-05-25T15:16:17Z"',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithInteger(){
+        $plist = new PropertyList;
+        $plist->setObject(1234);
+
+        self::assertEquals('"1234"',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('<*I1234>',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><integer>1234</integer></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('1234',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithReal(){
+        $plist = new PropertyList;
+        $plist->setObject(12.34);
+
+        self::assertEquals('"12.34"',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('<*R12.34>',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><real>12.34</real></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('12.34',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithString(){
         $plist = new PropertyList;
         $plist->setObject('Hello there');
 
@@ -27,6 +127,30 @@ class PropertyListTest extends TestCase{
         self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><string>Hello there</string></plist>',$plist->serialize(PropertyList::FORMAT_XML));
         //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
         self::assertEquals('"Hello there"',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithArray(){
+        $plist = new PropertyList;
+        $plist->setObject([true,false,new DateTime('2023-05-25 15:16:17 +00:00'),1234,12.34,'Hello there']);
+
+        self::assertEquals('("true", "false", "2023-05-25 15:16:17 Z", "1234", "12.34", "Hello there")',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('(<*BY>, <*BN>, <*D2023-05-25 15:16:17 Z>, <*I1234>, <*R12.34>, "Hello there")',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><array><true/><false/><date>2023-05-25T15:16:17Z</date><integer>1234</integer><real>12.34</real><string>Hello there</string></array></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('[true,false,"2023-05-25T15:16:17Z",1234,12.34,"Hello there"]',$plist->serialize(PropertyList::FORMAT_JSON));
+    }
+
+    public function testSerializingPropertyListWithDictionary(){
+        $plist = new PropertyList;
+        $plist->setObject((object) ['true key'=>true,'false key'=>false,'date key'=>new DateTime('2023-05-25 15:16:17 +00:00'),'integer key'=>1234,'real key'=>12.34,'string key'=>'Hello there']);
+
+        self::assertEquals('{"true key" = "true"; "false key" = "false"; "date key" = "2023-05-25 15:16:17 Z"; "integer key" = "1234"; "real key" = "12.34"; "string key" = "Hello there";}',$plist->serialize(PropertyList::FORMAT_ASCII));
+        self::assertEquals('{"true key" = <*BY>; "false key" = <*BN>; "date key" = <*D2023-05-25 15:16:17 Z>; "integer key" = <*I1234>; "real key" = <*R12.34>; "string key" = "Hello there";}',$plist->serialize(PropertyList::FORMAT_ASCII_GNUSTEP));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY_GNUSTEP));
+        self::assertEquals('<plist xlmns="https://www.apple.com/DTDs/PropertyList-1.0.dtd" version="1.0"><dict><key>true key</key><true/><key>false key</key><false/><key>date key</key><date>2023-05-25T15:16:17Z</date><key>integer key</key><integer>1234</integer><key>real key</key><real>12.34</real><key>string key</key><string>Hello there</string></dict></plist>',$plist->serialize(PropertyList::FORMAT_XML));
+        //self::assertEquals('',$plist->serialize(PropertyList::FORMAT_BINARY));
+        self::assertEquals('{"true key":true,"false key":false,"date key":"2023-05-25T15:16:17Z","integer key":1234,"real key":12.34,"string key":"Hello there"}',$plist->serialize(PropertyList::FORMAT_JSON));
     }
 
 }
